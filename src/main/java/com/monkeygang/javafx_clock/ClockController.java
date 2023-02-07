7c08980d9e582677bb30788a6279e07b103029bc
sunwar01 <sunwar01@easv365.dk>
2023-02-07 14:12:06 +0100

    Rootate cock functionality

--- src/main/java/com/monkeygang/javafx_clock/ClockController.java
+++ src/main/java/com/monkeygang/javafx_clock/ClockController.java
@@ -3,11 +3,12 @@ package com.monkeygang.javafx_clock;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.transform.Rotate;
 
 public class ClockController {
 
 
-    getTime getTimeObject = new getTime();
+
 
 
     @FXML
@@ -16,23 +17,41 @@ public class ClockController {
     @FXML
     private Line sTest;
 
+    public void test(){
+
+        System.out.println("test");
+    }
+
+    Rotate rotate = new Rotate();
+    double up = 0.0;
 
     public void RootateSecondLine(){
+
+        getTime getTimeObject = new getTime();
+
+
+
+        rotate.setAngle(getTimeObject.getSeconds() * 6);
+        System.out.println(getTimeObject.getSeconds());
+        rotate.setPivotX(18);
+        rotate.setPivotY(2);
+        sTest.getTransforms().addAll(rotate);
+
 
 
     }
 
 
     public void initialize(){
-
+        /*
         getTimeObject.getSeconds();
         System.out.println("Seconds: " + getTimeObject.getSeconds());
         getTimeObject.getMinutes();
         System.out.println("Minutes: " + getTimeObject.getMinutes());
         getTimeObject.getHours();
         System.out.println("Hours: " + getTimeObject.getHours());
-
-
+        */
+        RootateSecondLine();
     }
 
 }
